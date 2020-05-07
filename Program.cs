d2ec93b960831c11fce7803e2adf1fa4380f5a33
Michał Zaorski <michal.zaorski98@gmail.com>
2020-05-07 21:11:08 +0200

    generating keys for DES

--- Program.cs
+++ Program.cs
@@ -5,6 +5,23 @@ namespace CryptoAlgorithms
 {
     class Program
     {
+
+        private static int[] key_generating_permutation_1 = new int[]
+        {
+            57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4
+        };
+
+        private static int[] shift_array = new int[]
+        {
+            1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1
+        };
+
+        private static int[] key_generating_permutation_2 = new int[]
+        {
+            14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32
+        };
+
+
         static void Main(string[] args)
         {
             //HEREISASECRETMESSAGEENCIPHEREDBYTRANSPOSITION
@@ -46,15 +63,24 @@ namespace CryptoAlgorithms
             //Console.WriteLine();
 
             //LSFR
-            Console.WriteLine("-------------LFSR Generator-------------");
-            int[] taps = new int[2] { 1, 4 };
+            //Console.WriteLine("-------------LFSR Generator-------------");
+            //int[] taps = new int[2] { 1, 4 };
             //String number = LFSR("1101", taps);
             //Console.WriteLine("Generated Number with LFSR: " + number);
             //SynchronousEncode("11101001", "0010", taps);
             //SynchronousDecode("10010011", "0010", taps);
             //AutokeyEncode("11101001", "0011", taps);
-            AutokeyDecode("0110100011100101", "0011", taps);
-            Console.WriteLine();
+            //AutokeyDecode("0110100011100101", "0011", taps);
+            //Console.WriteLine();
+
+            //DES
+            //Console.WriteLine("-------------DES-------------");
+            List<String> keys = DESKeyGenerate("IEOFIT#1");
+            int idx = 1;
+            foreach(String s in keys)
+            {
+                Console.WriteLine("Key " + idx++ + " : " + s);
+            }
 
         }
 
@@ -892,6 +918,63 @@ namespace CryptoAlgorithms
             Console.WriteLine("Wynil: " + result);
 
         }
+
+
+        public static List<String> DESKeyGenerate (String key)
+        {
+
+            List<String> keys = new List<String>();
+
+            String binary_key = "";
+            foreach(char c in key)
+            {
+                binary_key += Convert.ToString(c, 2).PadLeft(8, '0');
+            }
+
+            //Console.WriteLine("Given key in binary: " + binary_key);
+            String permutated_key = "";
+            for (int i = 0; i < key_generating_permutation_1.Length; i++)
+            {
+                permutated_key += binary_key[key_generating_permutation_1[i] - 1];
+            }
+
+            //Console.WriteLine("Permutated Key : " + permutated_key);
+
+            String left_key_part = permutated_key.Substring(0,permutated_key.Length / 2);
+            String right_key_part = permutated_key.Substring(permutated_key.Length / 2, permutated_key.Length / 2);
+
+            Console.WriteLine("Left part of the key: " + left_key_part);
+            Console.WriteLine("Right part of the key: " + right_key_part);
+            Console.WriteLine();
+
+            String left_value;
+            String right_value;
+            String permutated_new_key;
+            for (int i = 0; i < shift_array.Length; i++)
+            {
+                left_value = left_key_part.Substring(0,shift_array[i]);
+                left_key_part = left_key_part.Remove(0, shift_array[i]);
+                left_key_part = left_key_part.Insert(left_key_part.Length, left_value.ToString());
+
+                right_value = right_key_part.Substring(0, shift_array[i]);
+                right_key_part = right_key_part.Remove(0, shift_array[i]);
+                right_key_part = right_key_part.Insert(right_key_part.Length, right_value.ToString());
+
+                String new_key = left_key_part + right_key_part;
+
+                permutated_new_key = "";
+                for (int j = 0; j < key_generating_permutation_2.Length; j++)
+                {
+                    permutated_new_key += new_key[key_generating_permutation_2[j] - 1];
+                }
+
+                keys.Add(permutated_new_key);
+
+            }
+
+            return keys;
+        }
+
 
     }
 }
